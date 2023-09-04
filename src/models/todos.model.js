3c616f334f06ddd02150070bb3b55e3ba49546ea
GerardoAcosta1 <gerardonuevo1234321@gmail.com>
2023-09-03 21:29:10 -0700

    server created

---
+++ src/models/todos.model.js
@@ -0,0 +1,25 @@
+import { DataTypes } from "sequelize";
+import db from "../utils/dataBase.js";
+
+const todos = db.define('todos',{
+
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement: true,
+    },
+    title:{
+        type: DataTypes.STRING(40),
+        allowNull: false,
+       
+    },
+    description:{
+        type: DataTypes.STRING(250),
+        allowNull: false,
+    },
+    completed:{
+        type: DataTypes.BOOLEAN({ multi: true }),
+        allowNull: false,
+    }
+});
+export default todos;
