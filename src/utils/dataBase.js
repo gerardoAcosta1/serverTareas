d1d035cbf8a2a849170277197b4136827d492a8c
GerardoAcosta1 <gerardonuevo1234321@gmail.com>
2023-09-03 21:44:53 -0700

    seagregó databasetareas

--- src/utils/dataBase.js
+++ src/utils/dataBase.js
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize"
 
 const db = new Sequelize(
     {
-        host: 'localhost',
-        username: 'postgres',
-        database: 'tareas',
+        host: 'dpg-cjpa6iojbais7381tk10-a',
+        username: 'tareas_ux36_user',
+        database: 'tareas_ux36',
         port: '5432',
-        password: 'root',
+        password: 'lpvu6kGHS6syOoE4FAy1v2GNtzFPC1rT',
         dialect: 'postgres'
     }
 );
